a767874b412c5595170462f12aa9933dede3336a
MOUSSA THIOR <netrathior@gmail.com>
2024-06-17 03:23:06 +0000

    Add Mapper class for DTOs

---
+++ src/main/java/uacd/master/sir/fast_food_api/utils/Mapper.java
@@ -0,0 +1,38 @@
+package uacd.master.sir.fast_food_api.utils;
+
+import uacd.master.sir.fast_food_api.dto.RoleUtilisateurRequestDTO;
+import uacd.master.sir.fast_food_api.dto.UtilisateurRequestDTO;
+import uacd.master.sir.fast_food_api.dto.UtilisateurResponseDTO;
+import uacd.master.sir.fast_food_api.models.Roleutilisateur;
+import uacd.master.sir.fast_food_api.models.Utilisateur;
+
+public class Mapper {
+
+    public static UtilisateurResponseDTO mapToUtilisateurResponse(Utilisateur utilisateur, UtilisateurResponseDTO responseDTO){
+
+        responseDTO.setId(utilisateur.getIdutilisateur());
+        responseDTO.setPrenom(utilisateur.getPrenom());
+        responseDTO.setNom(utilisateur.getNom());
+        responseDTO.setEmail(utilisateur.getEmail());
+
+        return responseDTO;
+    }
+
+    public static Utilisateur mapToEntityUtilisateur(Utilisateur utilisateur, UtilisateurRequestDTO requestDTO){
+
+        utilisateur.setPrenom(requestDTO.getPrenom());
+        utilisateur.setNom(requestDTO.getNom());
+        utilisateur.setEmail(requestDTO.getEmail());
+        utilisateur.setPassword(requestDTO.getPassword());
+
+        return utilisateur;
+    }
+
+    public static Roleutilisateur mapToEntityRoleutilisateur(Roleutilisateur roleutilisateur, RoleUtilisateurRequestDTO requestDTO){
+
+        roleutilisateur.setUtilisateurByIdutilisateur(requestDTO.getUtilisateur());
+        roleutilisateur.setRoleByIdrole(requestDTO.getRole());
+
+        return roleutilisateur;
+    }
+}
