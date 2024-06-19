2bad24ffcecf8bd2ff5196810c0c8d644aed52d5
Mamadou SENE <senemamadou1999@gmail.com>
2024-06-19 00:28:55 +0000

    ajout des dto pour produit

---
+++ src/main/java/uacd/master/sir/fast_food_api/DTO/ProduitRequestDTO.java
@@ -0,0 +1,15 @@
+package uacd.master.sir.fast_food_api.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigInteger;
+@Getter
+@Setter
+public class ProduitRequestDTO {
+    private int idcategorie;
+    private String nom;
+    private String description;
+    private BigInteger prix;
+    private String image;
+}
---
+++ src/main/java/uacd/master/sir/fast_food_api/DTO/ProduitResponseDTO.java
@@ -0,0 +1,20 @@
+package uacd.master.sir.fast_food_api.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigInteger;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProduitResponseDTO {
+    private int idproduit;
+    private int idCategorie;
+    private String nom;
+    private String description;
+    private BigInteger prix;
+    private String image;
+}
