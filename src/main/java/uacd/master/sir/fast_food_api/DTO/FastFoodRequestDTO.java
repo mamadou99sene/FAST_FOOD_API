dae06ef8e6550fcd1e129d1adbe10c5347641d9c
Mamadou SENE <senemamadou1999@gmail.com>
2024-06-20 17:06:58 +0000

    ajout du request dto pour fast food

---
+++ src/main/java/uacd/master/sir/fast_food_api/DTO/FastFoodRequestDTO.java
@@ -0,0 +1,13 @@
+package uacd.master.sir.fast_food_api.DTO;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+public class FastFoodRequestDTO {
+    private int idutilisateur;
+    private String nom;
+    private String description;
+    private String localisation;
+}
