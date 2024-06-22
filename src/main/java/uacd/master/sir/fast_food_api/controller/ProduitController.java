cd9968965df9d5b4648d0678112caba019521e47
Mamadou SENE <senemamadou1999@gmail.com>
2024-06-22 22:05:32 +0000

    redefinition de la methode de suppression d'un produit

--- src/main/java/uacd/master/sir/fast_food_api/controller/ProduitController.java
+++ src/main/java/uacd/master/sir/fast_food_api/controller/ProduitController.java
@@ -55,8 +55,8 @@ public class ProduitController {
         boolean deleted=produitService.deleteProduit(idProduit);
         if(!deleted)
         {
-            return new ResponseEntity(HttpStatus.NO_CONTENT);
+            return new ResponseEntity(HttpStatus.GONE);
         }
-        return new ResponseEntity(deleted,HttpStatus.GONE);
+        return new ResponseEntity(deleted,HttpStatus.OK);
     }
 }
